44813e24478877040dad9f00628203287f6692c1
K Burnett <bluemalek@gmail.com>
2023-03-06 11:48:20 +0000

    added base files

--- src/server.js
+++ src/server.js
@@ -4,12 +4,10 @@ const port = process.env.PORT || 5001;
 // PORT 80 is default for localhost, in routes for request, 80 does not
 // need to be specified
 
-const exRouter = require("../middleware");
 
 const app = express();
 app.use(express.json());
 
-app.use(exRouter);
 
 app.get("/health", (req, res) => 
     res.status(200).json({message: "API is working"
